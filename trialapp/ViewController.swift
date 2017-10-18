a46e16304498da9b909c1a20ea40c660ad9d76e9
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-18 23:11:03 +0530

    minor changes regarding screen building & stack view implementation

--- trialapp/ViewController.swift
+++ trialapp/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgimg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // To auto layout an app images to any ios device following code will be written
+        
+        //swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        //bgimg.frame = view.frame
+        
     }
 
     override func didReceiveMemoryWarning() {
