76bf3e4e6cf70e252ec62e30ffbeb242020e0a4b
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-19 01:36:58 +0530

    aboutIbOutlet Details

---
+++ trialapp/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  trialapp
+//
+//  Created by Bibhuranjan Bihari on 19/10/17.
+//  Copyright © 2017 Bibhuranjan Bihari. All rights reserved.
+//
+
+import UIKit
+
+struct Player
+{
+    var desiredLeague : String?
+    var selectedSkillLevel : String?
+}
