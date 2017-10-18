832f07d0d9490a31964c3e962083a6a500e92942
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-18 20:02:17 +0530

    Commit after first lesson video watching--Creating the starting application

---
+++ trialapp/Views/BorderButton.swift
@@ -0,0 +1,19 @@
+//
+//  BorderButton.swift
+//  trialapp
+//
+//  Created by Bibhuranjan Bihari on 18/10/17.
+//  Copyright © 2017 Bibhuranjan Bihari. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
